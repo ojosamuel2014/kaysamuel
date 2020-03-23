6287699527202c5bb8ad866e415d2fb7b2cf6fef
ojosamuel2014 <62174897+ojosamuel2014@users.noreply.github.com>
2020-03-24 00:59:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = 'Samuel Ojo';
+console.log("My Name is: " + name);
+
+console.log("My Courses are: ");
+var courseName = ["HTML", "CSS", "JavaScript"];
+console.log(courseName);
+
+console.log("All odd numbers from 1 - 200 are: ");
+
+for (var x = 0; x <= 200; x++) {
+    if (x % 2 !== 0) {
+        console.log(x + " is an odd number");
+    }
+
+}
